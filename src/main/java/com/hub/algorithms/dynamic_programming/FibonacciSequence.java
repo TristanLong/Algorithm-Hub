8cb9a10f3566eb50deada7ec518d9b0dfa9267a6
Tristan Long <trieukimlong102000@gmail.com>
2026-03-14 18:43:08 +0700

    theory: add time complexity of fibonacci sequence

--- src/main/java/com/hub/algorithms/dynamic_programming/FibonacciSequence.java
+++ src/main/java/com/hub/algorithms/dynamic_programming/FibonacciSequence.java
@@ -10,6 +10,11 @@ public class FibonacciSequence {
         Sử dụng một mảng để lưu trữ các giá trị Fibonacci.
         Bắt đầu với các trường hợp cơ sở (base cases): Fib(0) = 0, Fib(1) = 1.
         Đối với mỗi giá trị tiếp theo, tính Fib(n) = Fib(n-1) + Fib(n-2) và lưu trữ nó.
+
+        Time Complexity:
+        - Best-case: O(n) - Khi n nhỏ hơn hoặc bằng 1, thuật toán trả về kết quả ngay lập tức.
+        - Average-case: O(n) - Khi n lớn hơn 1, thuật toán phải tính toán tất cả các giá trị từ 2 đến n.
+        - Worst-case: O(n) - Khi n lớn hơn 1, thuật toán phải tính toán tất cả các giá trị từ 2 đến n.
      */
 
     public static int fibonacci(int n) {
